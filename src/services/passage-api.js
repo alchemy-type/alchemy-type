94c99be8668b2c13cb7de8c6e6c5fca5bfc6fa84
Jackmc <jackmc1122@gmail.com>
2019-05-13 14:41:28 -0700

    Change the api to save passages in an array and add get by id method.

--- src/services/passage-api.js
+++ src/services/passage-api.js
@@ -1,13 +1,27 @@
 const passageApi = {
     key: 'passage',
     save(passage) {
-        const passageData = JSON.stringify(passage);
+        let passages = passageApi.getAll();
+        passages.push(passage);
+        const passageData = JSON.stringify(passages);
         localStorage.setItem(passageApi.key, passageData);
+    },
+    get(id) {
+        const passages = passageApi.getAll();
+        for(let i = 0; i < passages.length; i++) {
+            let passage = passages[i];
+            if(passage.id === id) {
+                return passage;
+            }
+        }
     },
     getAll() {
         const passageData = localStorage.getItem(passageApi.key);
-        const passage = JSON.parse(passageData);
-        return passage;
+        let passages = JSON.parse(passageData);
+        if(!passages) {
+            passages = [];
+        }
+        return passages;
     }
 };
 
--- test/passage-api.test.js
+++ test/passage-api.test.js
@@ -6,17 +6,18 @@ QUnit.module('passage-api');
 const key = 'test-passages';
 passageApi.key = key;
 
-test('Test saving a passage', assert => {
+test('Test saving a passage and get it back', assert => {
     localStorage.removeItem(key);
     //Arrange
     const passage = {
-        id: 'passage',
+        id: 'JavaScriptpassage',
+        language: 'JavaScript',
         title: 'Passage Title',
         text: 'This is a test passage.\nThis is a second line'
     };
     //Act 
     passageApi.save(passage);
-    const result = passageApi.getAll();
+    const result = passageApi.get(passage.id);
     //Assert
     assert.deepEqual(result, passage);
 });
