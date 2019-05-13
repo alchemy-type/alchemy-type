a13d88c99cf29126f4b14ec40e0607da1bdfc109
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-13 10:23:34 -0700

    added event listener and created array from userInput

--- src/practice/practice.js
+++ src/practice/practice.js
@@ -2,11 +2,18 @@ import passages from '../services/passage-data.js';
 import createSpans from './create-spans.js';
 
 let passageParent = document.getElementById('passage-characters');
+let userInput = document.getElementById('passage-input');
 
 const passage = passages[0];
 const passageText = passage.text;
 createSpans(passageText, passageParent);
 
 const passageArray = Array.from(passageText);
+const emptyArray = Array(passageText.length);
 
+let userInputCharaters = 0;
 
+userInput.addEventListener('input', (event) => {
+    emptyArray[userInputCharaters] = event.target.value[userInputCharaters];
+    userInputCharaters = event.target.value.length;
+});
