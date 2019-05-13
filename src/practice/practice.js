0cfc3599f2752c4cbab53133b0a5975286c11643
Jackmc <jackmc1122@gmail.com>
2019-05-13 11:03:33 -0700

    Rename charaterCount variable and reset matchFlag to true when the input and passage match.

--- src/practice/practice.js
+++ src/practice/practice.js
@@ -11,19 +11,22 @@ createSpans(passageText, passageParent);
 const passageArray = Array.from(passageText);
 const emptyArray = Array(passageText.length);
 
-let userInputCharaters = 0;
+let userInputLength = 0;
 let matchFlag = true;
 
+
 userInput.addEventListener('input', (event) => {
-    emptyArray[userInputCharaters] = event.target.value[userInputCharaters];
-    userInputCharaters = event.target.value.length;
-    if(emptyArray[userInputCharaters - 1] !== passageArray[userInputCharaters - 1]) {
+    emptyArray[userInputLength] = event.target.value[userInputLength];
+    userInputLength = event.target.value.length;
+    if(emptyArray[userInputLength - 1] !== passageArray[userInputLength - 1]) {
         matchFlag = false;
+    } else {
+        matchFlag = true;
     }
 });
 
 userInput.addEventListener('keydown', event => {
-    if(!matchFlag) {
+    if(!matchFlag && event.code !== 'Backspace') {
         event.preventDefault();
     }
 });
