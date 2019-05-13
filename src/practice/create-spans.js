9f7c9e11fdd5201d353d3f4b0a66635fce93ce4b
Jackmc <jackmc1122@gmail.com>
2019-05-13 13:20:04 -0700

    Add second test passage with multiline and handle newline with a br tag in create spans.

--- src/practice/create-spans.js
+++ src/practice/create-spans.js
@@ -2,6 +2,10 @@ function createSpans(passageText, parent) {
     for(let i = 0; i < passageText.length; i++) {
         const span = document.createElement('span');
         span.textContent = passageText.charAt(i);
+        if(passageText.charAt(i) === '\n') {
+            const br = document.createElement('br');
+            span.appendChild(br);
+        }
         parent.appendChild(span);
     }
     return parent;
--- src/services/passage-data.js
+++ src/services/passage-data.js
@@ -4,6 +4,12 @@ const testPassage = {
     text: 'This is a text passage.'
 };
 
-const passages = [testPassage];
+const multilineTestPassage = {
+    id: 'multiline',
+    title: 'Multiline Test Passage',
+    text: 'This is a test passage.\nThis is a second line'
+};
+
+const passages = [testPassage, multilineTestPassage];
 
 export default passages;
