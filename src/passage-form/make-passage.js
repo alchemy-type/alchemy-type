e59110e7ceb0ae7b4cdd1887410850facceda72a
Jackmc <jackmc1122@gmail.com>
2019-05-14 09:52:55 -0700

    Update id to use current date instead of language.

--- src/passage-form/make-passage.js
+++ src/passage-form/make-passage.js
@@ -1,9 +1,10 @@
 function makePassage(formData) {
     const passage = {
-        id: formData.get('language') + formData.get('title'),
+        id: formData.get('title') + formData.get('date'),
         language: formData.get('language'),
         title: formData.get('title'),
-        text: formData.get('text')
+        text: formData.get('text'),
+        date: formData.get('date')
     };
     return passage;
 }
--- test/make-passage.test.js
+++ test/make-passage.test.js
@@ -5,11 +5,13 @@ QUnit.module('make passage');
 
 test('Test saving a passage and get it back', assert => {
     //Arrange
+    const date = Date.now();
     const passage = {
-        id: 'JavaScriptpassage',
+        id: 'passage' + date,
         language: 'JavaScript',
         title: 'passage',
-        text: 'This is a test passage.\nThis is a second line'
+        text: 'This is a test passage.\nThis is a second line',
+        date: date.toString()
     };
 
     const formData = new FormData();
@@ -17,6 +19,7 @@ test('Test saving a passage and get it back', assert => {
     formData.set('language', passage.language);
     formData.set('title', passage.title);
     formData.set('text', passage.text);
+    formData.set('date', date);
 
     //Act 
     const result = makePassage(formData);
