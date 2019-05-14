2b4d4c547672f49152255ea20ab5169bc00d10fa
Jackmc <jackmc1122@gmail.com>
2019-05-13 17:06:15 -0700

    TDD handleCurrentChar function to highlight character user is supposed to type.

--- src/practice/practice.js
+++ src/practice/practice.js
@@ -1,6 +1,7 @@
 import passages from '../services/passage-data.js';
 import createSpans from './create-spans.js';
 import handleMatchFlag from './handle-match-flag.js';
+import handleCurrentChar from './handle-current-char.js';
 
 let passageParent = document.getElementById('passage-characters');
 let userInput = document.getElementById('passage-input');
@@ -15,8 +16,11 @@ const emptyArray = Array(passageText.length);
 let userInputLength = 0;
 let matchFlag = true;
 
+let currentChar = handleCurrentChar(passageParent, passageParent.children[0], userInputLength);
+
 userInput.addEventListener('input', (event) => {
     userInputLength = event.target.value.length;
+    currentChar = handleCurrentChar(passageParent, currentChar, userInputLength);
     emptyArray[userInputLength - 1] = event.target.value[userInputLength - 1];
     matchFlag = handleMatchFlag(emptyArray, passageArray, userInputLength);
 });
--- test/tests.js
+++ test/tests.js
@@ -1,2 +1,3 @@
 import './create-spans.test.js';
 import './handle-match-flag.test.js';
+import './handle-current-char.test.js';
