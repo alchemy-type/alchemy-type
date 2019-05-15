408f713c694a2e57124a61c0545c5a30cefeba8e
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-05-15 08:41:50 -0700

    created working stopwatch

---
+++ src/services/stopwatch.js
@@ -0,0 +1,23 @@
+var textInput = document.getElementById('passage-input');
+
+var minutesLabel = document.getElementById('minutes');
+var secondsLabel = document.getElementById('seconds');
+var totalSeconds = 0;
+setInterval(stopWatch, 1000);
+
+function stopWatch() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = time(totalSeconds % 60);
+    minutesLabel.innerHTML = time(parseInt(totalSeconds / 60));
+}
+
+function time(time) {
+    var timeString = time + '';
+    if(timeString.length < 2) {
+        return '0' + timeString;
+    } else {
+        return timeString;
+    }
+}
+
+export default time;
