d74806753f204b0a8c0339b86266ff1822c3ba59
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-15 14:28:25 -0700

    Added stop-watch.js and imported into practice.js

--- src/practice/practice.js
+++ src/practice/practice.js
@@ -4,9 +4,12 @@ import handleCurrentChar from './handle-current-char.js';
 import handleErrorChar from './handle-error-char.js';
 import checkEndGame from './checkEndGame.js';
 import passageApi from '../services/passage-api.js';
+import { stopWatch, reset } from '../services/stop-watch.js';
 
 let passageParent = document.getElementById('passage-characters');
 let userInput = document.getElementById('passage-input');
+let minutesLabel = document.getElementById('minutes');
+let secondsLabel = document.getElementById('seconds');
 
 const searchParams = new URLSearchParams(window.location.search);
 const passageId = searchParams.get('id');
@@ -23,16 +26,23 @@ let matchFlag = true;
 let gameOver = false;
 let enterFlag = false;
 
+let timer = null;
+
 let currentChar = handleCurrentChar(passageParent, passageParent.children[0], userInputLength);
 
 userInput.addEventListener('input', (event) => {
     emptyArray.push(event.target.value);
     userInputLength = emptyArray.length;
+    if(userInputLength === 1) {
+        timer = setInterval(stopWatch, 1000);
+    }
     userInput.value = '';
     gameOver = checkEndGame(passageArray, emptyArray);
 
     if(!gameOver) {
         currentChar = handleCursor(emptyArray, passageArray, passageParent, currentChar, userInputLength);
+    } else {
+        clearInterval(timer);
     }
 
     if(enterFlag && matchFlag) {
@@ -50,6 +60,12 @@ userInput.addEventListener('keydown', event => {
     } else if(event.code === 'Backspace') {
         emptyArray.pop();
         userInputLength = emptyArray.length;
+        if(userInputLength === 0) {
+            reset();
+            clearInterval(timer);
+            minutesLabel.textContent = '00';
+            secondsLabel.textContent = '00';
+        }
         currentChar.classList.remove('typed');
         currentChar = handleCursor(emptyArray, passageArray, passageParent, currentChar, userInputLength);
     }
