a3f223c518989a4f68012f6d1bf792b90765ed7a
Jackmc <jackmc1122@gmail.com>
2019-05-15 16:40:17 -0700

    Refactor practice.js and test handleEnter().

---
+++ src/practice/handle-cursor.js
@@ -0,0 +1,20 @@
+import handleMatchFlag from './handle-match-flag.js';
+import handleCurrentChar from './handle-current-char.js';
+import handleErrorChar from './handle-error-char.js';
+
+function handleCursor(emptyArray, passageArray, passageParent, currentChar, userInputLength, matchFlag) {
+    matchFlag = handleMatchFlag(emptyArray, passageArray, userInputLength);
+    if(matchFlag) {
+        currentChar = handleCurrentChar(passageParent, currentChar, userInputLength);
+    }
+    handleErrorChar(matchFlag, passageParent, currentChar, userInputLength);
+
+    // Return object so we can get both the currentChar and the matchFlag back in the main file
+    const cursorObj = {
+        currentChar: currentChar,
+        matchFlag: matchFlag
+    };
+    return cursorObj;
+}
+
+export default handleCursor;
