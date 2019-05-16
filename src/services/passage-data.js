f631cc7e80780b2fdcc03507063b428d5f62cbb5
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-05-16 11:35:55 -0700

    fixed failing test create-passage-link.test.js

--- src/services/passage-data.js
+++ src/services/passage-data.js
@@ -1,12 +1,14 @@
 const testPassage = {
     id: 'test',
     title: 'Test Passage',
+    description: 'Test a Passage Input',
     text: 'This is a text passage.'
 };
 
 const multilineTestPassage = {
     id: 'multiline',
     title: 'Multiline Test Passage',
+    description: 'Multiline Test Passage',
     text: 'This is a test passage.\nThis is a second line'
 };
 
