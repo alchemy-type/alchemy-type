65240f9201ddff63fa5de8268eca4277ae945edf
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-16 11:13:04 -0700

    Wrote test for calculate wpm

---
+++ test/calc-wpm.test.js
@@ -0,0 +1,41 @@
+const test = QUnit.test;
+
+QUnit.module('calculate WPM');
+
+test('calculate wpm from emptyArray length and 60 seconds', assert => {
+    //arrange
+    let emptyArray = ['h', 'e', 'l', 'l', 'o', ' ', 'd', 'a', 'n', 'n', 'y'];
+
+    let totalSeconds = 60;
+
+    let expected = 2;
+    //act
+
+    const wpm = calcWPM(emptyArray, totalSeconds);
+
+    //assert
+    assert.equal(wpm, expected);
+});
+
+test('calculate wpm from emptyArray length and 120 seconds', assert => {
+    //arrange
+    let emptyArray = ['h', 'e', 'l', 'l', 'o', ' ', 'd', 'a', 'n', 'n', 'y'];
+
+    let totalSeconds = 120;
+
+    let expected = 1;
+    //act
+
+    const wpm = calcWPM(emptyArray, totalSeconds);
+
+    //assert
+    assert.equal(wpm, expected);
+});
+
+function calcWPM(emptyArray, totalSeconds) {
+    const words = emptyArray.length / 5.1;
+    const time = totalSeconds / 60;
+    const wpm = parseInt(words / time);
+
+    return wpm;
+}
