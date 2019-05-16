3fbbfbff08d59443936c22f429637e0a6b49fc1f
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-05-15 17:20:14 -0700

    added description to passage-list

--- src/passage-list/create-passage-link.js
+++ src/passage-list/create-passage-link.js
@@ -1,13 +1,16 @@
 function createPassageLink(passage) {
-    const section = document.createElement('section');
-    section.classList.add('passage-card');
+    const title = document.createElement('section');
+    const description = document.createElement('section');
+    title.classList.add('passage-card');
     const link = document.createElement('a');
     link.classList.add('passage');
     const searchParams = new URLSearchParams();
     searchParams.set('id', passage.id);
     link.href = 'practice.html?' + searchParams.toString();
-    section.textContent = passage.title;
-    link.appendChild(section);
+    title.textContent = passage.title;
+    description.textContent = passage.description;
+    link.appendChild(title);
+    link.appendChild(description);
     return link;
 }
 
