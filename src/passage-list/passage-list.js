58a66f5409e5228068234cab9f1699b0e0c0d291
Danny Hogan <hogan.daniel@hotmail.com>
2019-05-15 17:27:51 -0700

    Revert "Load hard coded passages to the begining of the passage list if they …"

--- src/passage-list/passage-list.js
+++ src/passage-list/passage-list.js
@@ -1,22 +1,8 @@
-import templatePassages from '../services/passage-data.js';
 import passageApi from '../services/passage-api.js';
 import createPassageLink from './create-passage-link.js';
 
 const passagesParent = document.getElementById('passage-list');
-
-let passages = passageApi.getAll();
-let templateArray = [];
-
-for(let i = 0; i < templatePassages.length; i++) {
-    const templatePassage = templatePassages[i];
-
-    if(!passageApi.get(templatePassage.id)) {
-        templateArray.push(templatePassage);
-        passageApi.save(templatePassage);
-    }
-}
-
-passages = templateArray.concat(passages);
+const passages = passageApi.getAll();
 
 for(let i = 0; i < passages.length; i++) {
     const passage = passages[i];
--- src/services/passage-data.js
+++ src/services/passage-data.js
@@ -1,38 +1,15 @@
-const functions = {
-    id: 'functions',
-    title: 'Javascript Functions',
-    text: `function anotherNiceFunction() {
-    return 'I'm another function!'
-}
-anotherNiceFunction
-function mathyFunction() {
-    return 3 * 4;
-}
-mathyFunction
-function anotherMathyFunction() {
-    return (3 * 3) - 6;
-}
-anotherMathyFunction
-function functionWithOneParameter(parameter) {
-    return parameter
-}`
+const testPassage = {
+    id: 'test',
+    title: 'Test Passage',
+    text: 'This is a text passage.'
 };
 
-const loops = {
-    id: 'loops',
-    title: 'Javascript Loops',
-    text: `for(let i = 0; i < 100; i++) {
-    console.log('The number is ', i);
-
-for(let i = 0; i <= 50; i += 2) {
-    console.log(i);
-}
-
-for(let i = 100; i > 0; i--) {
-    console.log(i);
-}`
+const multilineTestPassage = {
+    id: 'multiline',
+    title: 'Multiline Test Passage',
+    text: 'This is a test passage.\nThis is a second line'
 };
 
-const templatePassages = [functions, loops];
+const passages = [testPassage, multilineTestPassage];
 
-export default templatePassages;
+export default passages;
