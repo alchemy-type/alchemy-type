966dc4fc387854528c379ff6352b99085d382a49
Jackmc <jackmc1122@gmail.com>
2019-05-22 11:10:14 -0700

    Push the home page into a component to be broken appart later.

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import Component from './Component.js';
+
+class Home extends Component {
+
+    renderTemplate() {
+        return /*html*/`
+    <div>
+    <header>
+        <img id="logo" src="assets/alchemy-type.png">
+    </header>
+    <section>
+        <a href="./passage-list.html"><button>Practice your typing!</button></a>
+        <a href="./passage-form.html"><button>Add a new Passage!</button></a>
+        <a href="./stats.html"><button>View your stats!</button></a>
+    </section>
+    <footer>
+        <a href="./about-us.html"><button>About Us</button></a>
+    </footer>
+    </div>
+    `;
+    }
+
+}
+
+export default Home;
