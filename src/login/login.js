e1cddd0b8e1b51490dced16957cc86ea0a7dac04
Jackmc <jackmc1122@gmail.com>
2019-05-22 10:20:39 -0700

    Add onSubmitLogin functionality and save token on return successful login.

--- src/login/login.js
+++ src/login/login.js
@@ -1,1 +1,39 @@
-console.log('login');
+const form = document.getElementById('login-form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    const user = {
+        email: formData.get('email'),
+        password: formData.get('password'),
+    };
+
+    console.log(user);
+
+    onSubmitLogin(user);
+});
+
+const onSubmitLogin = (user) => {
+    event.preventDefault();
+    fetch('http://localhost:3000/login', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            email: user.email,
+            password: user.password
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            if(data.userId && data.success === 'true') {
+                console.log(data);
+                saveAuthToken(data.token);
+            }
+        });
+};
+
+const saveAuthToken = token => {
+    window.sessionStorage.setItem('token', token);
+};
