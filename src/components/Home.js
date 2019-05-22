0248eaac03e9286fb8070e57241afbbd4c0ab39e
Jackmc <jackmc1122@gmail.com>
2019-05-22 11:11:36 -0700

    Fix indentation.

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,17 +5,17 @@ class Home extends Component {
     renderTemplate() {
         return /*html*/`
     <div>
-    <header>
-        <img id="logo" src="assets/alchemy-type.png">
-    </header>
-    <section>
-        <a href="./passage-list.html"><button>Practice your typing!</button></a>
-        <a href="./passage-form.html"><button>Add a new Passage!</button></a>
-        <a href="./stats.html"><button>View your stats!</button></a>
-    </section>
-    <footer>
-        <a href="./about-us.html"><button>About Us</button></a>
-    </footer>
+        <header>
+            <img id="logo" src="assets/alchemy-type.png">
+        </header>
+        <section>
+            <a href="./passage-list.html"><button>Practice your typing!</button></a>
+            <a href="./passage-form.html"><button>Add a new Passage!</button></a>
+            <a href="./stats.html"><button>View your stats!</button></a>
+        </section>
+        <footer>
+            <a href="./about-us.html"><button>About Us</button></a>
+        </footer>
     </div>
     `;
     }
