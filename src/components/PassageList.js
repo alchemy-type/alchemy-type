b800c36445955c5468e0fa94036f109036dea109
Jackmc <jackmc1122@gmail.com>
2019-05-23 13:30:19 -0700

    Load passageLinks on the passageList component.

--- src/components/PassageList.js
+++ src/components/PassageList.js
@@ -2,6 +2,7 @@ import Component from './Component.js';
 import Logo from './Logo.js';
 import PassageLink from './PassageLink.js';
 import Footer from './Footer.js';
+import passageApi from '../services/passage-api.js';
 
 import templatePassages from '../services/passage-data.js';
 
@@ -16,15 +17,26 @@ class PassageList extends Component {
         const footer = new Footer();
         const footerDOM = footer.render();
 
-        const passageParent = dom.querySelector('section');
+        const passageParent = dom.querySelector('#passage-list');
 
         const main = dom.querySelector('main');
 
+        let passages = passageApi.getAll();
+        let templateArray = [];
+
         templatePassages.forEach(passage => {
+            if(!passageApi.get(passage.id)) {
+                templateArray.push(passage);
+                passageApi.save(passage);
+            }
+        });
+
+        passages = templateArray.concat(passages);
+
+        passages.forEach(passage => {
             const passageLink = new PassageLink({ passage });
             const passageLinkDOM = passageLink.render();
             passageParent.appendChild(passageLinkDOM);
-
         });
 
         dom.insertBefore(logoDOM, main);
