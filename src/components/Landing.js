be7daebe1d5c3f1cb3866da38d0c47bc71782d0e
Jackmc <jackmc1122@gmail.com>
2019-05-26 11:52:19 -0700

    Add landing page. Needs more styling.

---
+++ src/components/Landing.js
@@ -0,0 +1,45 @@
+import Component from './Component.js';
+import Header from './Header.js';
+import Logo from './Logo.js';
+import LandingNav from './LandingNav.js';
+import { checkAuth } from '../login/Login.js';
+
+class Landing extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+
+        const header = new Header({ user: this.state.user });
+        const headerDOM = header.render();
+
+        checkAuth((data) => {
+            this.state.user = data;
+            header.update({ user: this.state.user });
+        });
+
+        const logo = new Logo();
+        const logoDOM = logo.render();
+
+        const landingNav = new LandingNav();
+        const landingNavDOM = landingNav.render();
+
+        const main = dom.querySelector('main');
+
+        dom.prepend(headerDOM);
+        main.appendChild(logoDOM);
+        main.appendChild(landingNavDOM);
+
+        return dom;
+    }
+
+    renderTemplate() {
+        return /*html*/`
+        <div>
+            <main></main>
+        </div>
+    `;
+    }
+
+}
+
+export default Landing;
