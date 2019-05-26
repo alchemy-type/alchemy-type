e0627f895597b8f52d14292ac92f89a6756242ad
Jackmc <jackmc1122@gmail.com>
2019-05-25 18:35:35 -0700

    Don't show username if it isn't in the user object.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import Component from './Component.js';
 class Header extends Component {
 
     renderTemplate() {
-        const user = this.props.user || '';
+        const user = this.props.user || {};
         return /*html*/`
         <header>
             <h1>Alchemy Type</h1>
             <section class="user-header">
-                <h1>${user.name}</h1>
+                <h1>${user.name || ''}</h1>
                 <a href='./login.html'><h1>${user.email ? 'Logout' : 'Login'}</h1></a>
             </section>
         </header>
