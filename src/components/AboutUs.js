8b113e1d79ca1152fe2d9e38e409458c3952e8f8
Jackmc <jackmc1122@gmail.com>
2019-05-27 17:31:09 -0700

    Add header to about us page.

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,6 +1,9 @@
 import Component from './Component.js';
 import Logo from './Logo.js';
 import Footer from './Footer.js';
+import Header from './Header.js';
+
+import authApi from '../services/auth-api.js';
 
 class AboutUs extends Component {
 
@@ -13,55 +16,66 @@ class AboutUs extends Component {
         const footer = new Footer();
         const footerDOM = footer.render();
 
+        const header = new Header({ user: this.state.user });
+        const headerDOM = header.render();
+
+        authApi.checkAuth((data) => {
+            this.state.user = data;
+            header.update({ user: this.state.user });
+        });
+
         const main = dom.querySelector('main');
 
-        dom.insertBefore(logoDOM, main);
-        dom.appendChild(footerDOM);
+        dom.prepend(headerDOM);
+        main.prepend(logoDOM);
+        main.appendChild(footerDOM);
 
         return dom;
     }
 
     renderTemplate() {
         return /*html*/`
-        <section id="main-container">
-            <main>
-                <section class="box">
-                    <p class="name">Jack McConnell</p>
-                    <section class="icons-container">
-                        <a href="https://github.com/jwmcconnell"><img id="icons" src="/assets/logos/github.svg"
-                                alt="github-logo"></a>
-                        <a href="https://www.linkedin.com/in/mcconnelljack/"><img id="icons"
-                                src="/assets/logos/linkedin.svg" alt="linkedin-logo"></a>
-                        <a href="https://twitter.com/jackwmcconnell"><img id="icons" src="/assets/logos/twitter.svg"
-                                alt="twitter-logo"></a>
+        <div>
+            <main class="container">       
+                <section class="contributor-container">
+                    <section class="box">
+                        <p class="name">Jack McConnell</p>
+                        <section class="icons-container">
+                            <a href="https://github.com/jwmcconnell"><img id="icons" src="/assets/logos/github.svg"
+                                    alt="github-logo"></a>
+                            <a href="https://www.linkedin.com/in/mcconnelljack/"><img id="icons"
+                                    src="/assets/logos/linkedin.svg" alt="linkedin-logo"></a>
+                            <a href="https://twitter.com/jackwmcconnell"><img id="icons" src="/assets/logos/twitter.svg"
+                                    alt="twitter-logo"></a>
+                        </section>
                     </section>
-                </section>
 
-                <section class="box">
-                    <p class="name">Danny Hogan</p>
-                    <section class="icons-container">
-                        <a href="https://github.com/dannyhogan"><img id="icons" src="/assets/logos/github.svg"
-                                alt="github-logo"></a>
-                        <a href="https://www.linkedin.com/in/danny-hogan-3225b2183/"><img id="icons"
-                                src="/assets/logos/linkedin.svg" alt="linkedin-logo"></a>
-                        <a href="https://twitter.com/_DannyHogan"><img id="icons" src="/assets/logos/twitter.svg"
-                                alt="twitter-logo"></a>
+                    <section class="box">
+                        <p class="name">Danny Hogan</p>
+                        <section class="icons-container">
+                            <a href="https://github.com/dannyhogan"><img id="icons" src="/assets/logos/github.svg"
+                                    alt="github-logo"></a>
+                            <a href="https://www.linkedin.com/in/danny-hogan-3225b2183/"><img id="icons"
+                                    src="/assets/logos/linkedin.svg" alt="linkedin-logo"></a>
+                            <a href="https://twitter.com/_DannyHogan"><img id="icons" src="/assets/logos/twitter.svg"
+                                    alt="twitter-logo"></a>
+                        </section>
                     </section>
-                </section>
 
-                <section class="box">
-                    <p class="name">Eli Nicholson</p>
-                    <section class="icons-container">
-                        <a href="https://github.com/EmNicholson93"><img id="icons" src="/assets/logos/github.svg"
-                                alt="github-logo"></a>
-                        <a href="https://www.linkedin.com/in/eli-nicholson-87806062/"><img id="icons"
-                                src="/assets/logos/linkedin.svg" alt="linkedin-logo"></a>
-                        <a href="https://twitter.com/eli__nicholson"><img id="icons" src="/assets/logos/twitter.svg"
-                                alt="twitter-logo"></a>
+                    <section class="box">
+                        <p class="name">Eli Nicholson</p>
+                        <section class="icons-container">
+                            <a href="https://github.com/EmNicholson93"><img id="icons" src="/assets/logos/github.svg"
+                                    alt="github-logo"></a>
+                            <a href="https://www.linkedin.com/in/eli-nicholson-87806062/"><img id="icons"
+                                    src="/assets/logos/linkedin.svg" alt="linkedin-logo"></a>
+                            <a href="https://twitter.com/eli__nicholson"><img id="icons" src="/assets/logos/twitter.svg"
+                                    alt="twitter-logo"></a>
+                        </section>
                     </section>
-                </section>
+                </section>         
             </main>
-    </section>
+    </div>
     `;
     }
 
