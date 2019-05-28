b4c9d496db76b7ba3c8788261742c8e02bebdfdf
Jackmc <jackmc1122@gmail.com>
2019-05-27 18:29:30 -0700

    Send users with a token but are unauthorized to landing as well.

--- src/services/auth-api.js
+++ src/services/auth-api.js
@@ -11,10 +11,18 @@ const authApi = {
             })
                 .then(resp => resp.json())
                 .then(data => {
+                    if(data === 'Unauthorized') {
+                        authApi.redirectLogin();
+                    }
                     onLoadUser(data);
                 })
                 .catch('Error checking authorization');
-        } else if(window.location.pathname !== '/landing.html' &&
+        } else {
+            authApi.redirectLogin();
+        }
+    },
+    redirectLogin() {
+        if(window.location.pathname !== '/landing.html' &&
             window.location.pathname !== '/login.html' &&
             window.location.pathname !== '/register.html') {
             window.location = './landing.html';
